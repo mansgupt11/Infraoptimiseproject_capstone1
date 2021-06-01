9cf6da72da3bc3b19d56415638e27394f7763fcc
mansgupt11 <66791157+mansgupt11@users.noreply.github.com>
2021-06-01 22:35:12 +0530

    Update init-script-master.sh

--- infra-terraform-aws-base-instancecode/init-script-master.sh
+++ infra-terraform-aws-base-instancecode/init-script-master.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 apt update
-apt install apache2 -y
-apt install python3 -y
-apt update -y
 apt install software-properties-common
 add-apt-repository --yes --update ppa:ansible/ansible
 apt install ansible -y
-echo "Hi!, I am from $(hostname -f)" > /var/www/html/index.html
-systemctl apache2 start enable 
 hostnamectl set-hostname k8s-master
